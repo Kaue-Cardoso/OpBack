71deed530dbef874f2235df9d197235ba87e5159
Kauê Cardoso <kauekauada@hotmail.com>
2024-09-28 18:27:54 -0300

    Initial commit

---
+++ OpBack/src/main/java/OnePiece/OpBack/OpBackApplication.java
@@ -0,0 +1,13 @@
+package OnePiece.OpBack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpBackApplication.class, args);
+	}
+
+}
---
+++ OpBack/src/test/java/OnePiece/OpBack/OpBackApplicationTests.java
@@ -0,0 +1,13 @@
+package OnePiece.OpBack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
